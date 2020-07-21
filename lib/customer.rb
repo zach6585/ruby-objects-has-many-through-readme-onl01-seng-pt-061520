1a6ce3736ec670f953345c136cb6d6964b4db26b
zachary mines <zmines@wesleyan.edu>
2020-07-21 20:16:25 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,24 @@
 class Customer
+  @@all = []
+  attr_accessor :name, :age 
+  def initialize(name, age)
+    @name = name 
+    @age = age 
+    @@all << self 
+  end 
   
+  def self.all 
+    @@all 
+  end 
+  
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter,self, total, tip)
+  end 
+  def meals
+    Meal.all.select{|meal| meal.customer == self}
+  end 
+  
+  def waiters 
+    meals.waiters 
+  end 
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,13 @@
 class Waiter
-
+@@all = []
+  attr_accessor :name, :years 
+  def initialize(name, years)
+    @name = name 
+    @years = years 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end
 end
